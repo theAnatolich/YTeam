cf114f115ee70bef706b1001b92c2f4d4a140374
TheAnatolich <nullset1379@gmail.com>
2019-12-05 23:49:01 +0300

    piznec

--- cinema/src/main/java/com/YTeam/cinema/Application.java
+++ cinema/src/main/java/com/YTeam/cinema/Application.java
@@ -3,7 +3,8 @@ package com.YTeam.cinema;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+import java.sql.*;
+import java.util.logging.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,7 +12,7 @@ import java.sql.SQLException;
 @SpringBootApplication
 public class Application {
     /*wifi 192.168.0.107
-      cable
+      cable 192.168.0.103
     * */
     static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/cinema";
     static final String USER = "postgres";
@@ -21,6 +22,14 @@ public class Application {
 
         try (Connection connection = DriverManager.getConnection(DB_URL,USER,PASS)) {
             SpringApplication.run(Application.class, args);
+            Statement stat=connection.createStatement();
+//            stat.executeUpdate("INSERT INTO HALL (ID,NAME,HALL_TYPE_ID) values (1,'HALL1',1)");
+//            ResultSet rs= stat.executeQuery("select * from hall");
+//            while (rs.next()) {
+//
+//                System.out.println("HALL_ID : " + rs.getString("ID"));
+//            }
+
         } catch (SQLException e) {
             e.printStackTrace();
             return;
--- cinema/src/main/java/com/YTeam/cinema/GreetingController.java
+++ cinema/src/main/java/com/YTeam/cinema/GreetingController.java
@@ -1,14 +1,21 @@
 package com.YTeam.cinema;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @Controller
+
 public class GreetingController {
-    @GetMapping("/WEB-INF/greeting")
+    private final String sharedKey = "SHARED_KEY";
+
+    private static final String SUCCESS_STATUS = "success";
+    private static final String ERROR_STATUS = "error";
+    private static final int CODE_SUCCESS = 100;
+    private static final int AUTH_FAILURE = 102;
+
+    @GetMapping("/greeting")
     public String greeting(
             @RequestParam(name="name", required=false, defaultValue="World") String name,
             Map<String, Object> model
@@ -18,9 +25,10 @@ public class GreetingController {
     }
 
 
-    @GetMapping
+    @GetMapping("/")
     public String main(Map<String, Object> model) {
         model.put("some", "Sergo Loh");
         return "main";
     }
+
 }
