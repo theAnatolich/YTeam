4f50330a9f1fffcc923a197d39ff2e1800eb8ab8
TheAnatolich <nullset1379@gmail.com>
2019-12-08 00:05:55 +0300

    add data to page

--- cinema/src/main/java/com/YTeam/cinema/appController.java
+++ cinema/src/main/java/com/YTeam/cinema/appController.java
@@ -51,33 +51,48 @@ public class appController {
 
     @GetMapping("/SeetSelection")
     public String getSeetSelection(
-            @RequestParam(name="shedule_id", required=false, defaultValue="-1") int shedule_id,
+            @RequestParam(name="shedule_id", required=false, defaultValue="18") int shedule_id,
             Map<String, Object> model
     ) throws SQLException {
-        String q="select id,shedule_id,row_number,price,state from ticket  where shedule_id="+shedule_id;
+        String q="select id,shedule_id,plase_number,row_number,price,state from ticket  where shedule_id="+shedule_id;
         ArrayList<ticket> ticketList=new ArrayList<ticket>();
         ResultSet rs= stat.executeQuery(q);
         while (rs.next()){
-            ticketList.add(new ticket(  rs.getInt(1),
+            ticketList.add(
+                           new ticket(
+                                        rs.getInt(1),
                                         rs.getInt(2),
                                         rs.getInt(3),
                                         rs.getInt(4),
                                         rs.getInt(5),
-                                        rs.getInt(6)));
+                                        rs.getInt(6)
+                                      )
+                            );
         }
 //
 //        ArrayList<Integer> selectedTicket= new ArrayList<Integer>();
 //        selectedTicket.add(ticket_id);
 //        String ticketPurchase="update into ticket set state=1 where id="+ ticket_id;
 //        stat.executeUpdate(ticketPurchase);
-//
 //        String ticketReturn="";
 //        stat.executeUpdate(ticketReturn);
-
-
-
-        model.put("name", shedule_id);
+        model.put("name", ticketList);
         return "WEB-INF/pages/SeetSelection";
     }
 
+
+    @PostMapping("/SeetSelection")
+    public String getSeetSelection(
+            @RequestBody Integer[] ticket_id
+    ) throws SQLException {
+        String q="";
+        for (Integer id:ticket_id
+             ) {
+            if(q.length()==0) q="update into ticket set state=1 where id="+id;
+            else q+=" or id="+id;
+        }
+
+        stat.executeUpdate(q);
+        return "WEB_INF/pages/receipt";
+    }
 }
